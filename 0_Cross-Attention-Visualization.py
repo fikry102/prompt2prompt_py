e2f0de9982bccbceb181c5550cfde6904fd28d04
fikry102 <fikry102@qq.com>
2023-06-14 13:58:09 +0800

    add 0_Cross-Attention-Visualization

---
+++ 0_Cross-Attention-Visualization.py
@@ -0,0 +1,13 @@
+import torch
+from attention_control import AttentionStore,run_and_display,show_cross_attention
+
+
+#First let's generate an image and visualize the cross-attention maps for each word in the prompt. Notice, we normalize each map to 0-1.
+#Cross-Attention Visualization
+g_cpu = torch.Generator().manual_seed(8888) 
+prompts = ["A painting of a squirrel eating a burger"]
+controller = AttentionStore()
+image, x_t = run_and_display(prompts, controller, latent=None, run_baseline=False, generator=g_cpu)
+show_cross_attention(prompts,controller, res=16, from_where=("up", "down"))
+
+
